221d3a199fd66a73a1005b5a51c24b72d32aef75
patrickprakash <patrickprakash8@gmail.com>
2021-04-20 17:55:13 +0530

    First commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# aws-s3-file-upload
-This is a MERN Stack application for uploading file to the s3 bucket as part of Hubble hack
---
+++ server/app/config/s3.env.js
@@ -0,0 +1,8 @@
+const env = {
+	AWS_ACCESS_KEY: 'AKIA6NLE6FJ46NEPTVF4', 
+	AWS_SECRET_ACCESS_KEY: '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', 
+	REGION : 'us-east-1', 
+	Bucket: 'upload-hubble-hack-bucket' 
+};
+ 
+module.exports = env;
